09971861afd69b6b402e64688ec8e1f44d6c4f9c
Niklas Rasmusson <niklas.rasmusson@usify.se>
2015-11-03 21:00:48 +0100

    Add support for private channel a.k.a Groups and some logging

--- src/message-parser.js
+++ src/message-parser.js
@@ -12,16 +12,25 @@ function isAtSigned (message) {
 module.exports = {
   parseMessage: (message, slack) => {
     if (isAtSigned(message)) {
+      // TODO: add more logging, user etc
+      // TODO: message-parser should handle all parsing
       if (quotes.askedForQuote(message)) {
+        console.log('Asked for quote')
         switch (message.getChannelType()) {
           case 'DM':
             slack.dms[message.channel].send(quotes.getQuote())
+            console.log('Sending quote to DM channel')
             break
           case 'Channel':
             slack.channels[message.channel].send(quotes.getQuote())
+            console.log('Sending quote to Channel channel')
+            break
+          case 'Group':
+            slack.groups[message.channel].send(quotes.getQuote())
+            console.log('Sending quote to Group channel')
             break
           default:
-            console.log('Unknown channel type, cannot send quote.')
+            console.log('Unknown channel type, cannot send quote. Channel type: ' + message.getChannelType())
         }
       }
       /* else if () {} */ // More stuff
