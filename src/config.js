cdfea8fc6e93ed9cc0c22a454ddacc4fe7de4bab
Niklas Rasmusson <niklas.rasmusson@usify.se>
2015-11-03 19:07:35 +0100

    Change to use Heroku config vars

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
+'use strict'
+
 module.exports = {
-  SLACK_API_TOKEN: 'Token from Slack dashboard', // Add a bot at https://my.slack.com/services/new/bot and copy the token here.
+  SLACK_API_TOKEN: process.env.SLACK_KEY, // Add a bot at https://my.slack.com/services/new/bot and copy the token here.
   AUTO_CONNECT: true, // Automatically reconnect after an error response from Slack.
   AUTO_MARK: true // Automatically mark each message as read after it is processed.
 }
