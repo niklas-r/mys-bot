cd2b104dcb9af10d19dd1d3a4d5dbf43b4fd193e
Niklas Rasmusson <niklas.rasmusson@usify.se>
2015-11-03 20:34:41 +0100

    Add http server mainly because Heroku demands it

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 'use strict'
 
-let slackConnection = require('./src/slack-connection')
+const http = require('http')
+const slackConnection = require('./src/slack-connection')
 
 slackConnection.init()
+
+const PORT = process.env.PORT || 8000
+
+function handleRequest (req, res) {
+  res.end('It Works!')
+}
+
+const server = http.createServer(handleRequest)
+
+server.listen(PORT, () => {
+  console.log('Server listening on port %s', PORT)
+})
